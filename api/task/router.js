29f3574a3cae8530542f93d9e00771c48060feef
andrewskr90 <andrewskr90@gmail.com>
2021-09-10 14:17:24 -0700

    getTasks router and data call working

--- api/task/router.js
+++ api/task/router.js
@@ -1,5 +1,23 @@
 const express = require('express')
+const Task = require('./model')
 
 const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const tasks = await Task.getTasks()
+        res.status(200).json(tasks)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500)
+        .json({
+            message: err.message, 
+            stack: err.stack
+        })
+})
 
 module.exports = router
