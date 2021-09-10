506267911eae6422963f7875156a3aba1c4a007e
andrewskr90 <andrewskr90@gmail.com>
2021-09-10 12:55:01 -0700

    set up migration and seeds, getProjects working

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,33 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getProjects() {
+    const projects = await db('projects')
+
+    const projectsArray = projects.map(proj => {
+        const trueOrFalse = (integer) => {
+            if (integer === 0) {
+                return false
+            } else {
+                return true
+            }
+        }
+        return {
+            project_id: proj.project_id,
+            project_name: proj.project_name,
+            project_description: proj.project_description,
+            project_completed: trueOrFalse(proj.project_completed)
+        }
+    })
+
+    return projectsArray
+}
+
+async function insertProject(requestBody) {
+    const newProject = await db('projects').insert(requestBody)
+    return newProject
+}
+
+module.exports = {
+    getProjects,
+    insertProject
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,32 @@
-// build your `/api/projects` router here
+const express = require('express')
+const Project = require('./model')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    const projects = await Project.getProjects()
+    try {
+        res.status(200).json(projects)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    const project = await Project.insertProject(req.body)
+    try {
+        res.status(201).json(project)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500)
+        .json({
+            message: err.message, 
+            stack: err.stack
+        })
+})
+
+module.exports = router
